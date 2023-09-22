9291f65b322b39352eb3a8b25e4d1b06b03e2661
osamueldasilva <osamueldasilva1@gmail.com>
2023-09-22 20:59:53 -0300

    staging commit

--- src/Pages/PeoplePage/index.tsx
+++ src/Pages/PeoplePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { IResultsPeople } from "../../Interface/resultsPeoples";
 import { api } from "../../Service/apiStarWars";
 import * as S from "./style";
